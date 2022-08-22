a02b7a7d4010a76b5a46d256fbf07a8d1ff9eeaf
Chris Weyland <github@nextmail.cc>
2022-08-22 20:35:46 +0200

    move some files around

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='simplemailer',
-      version='1.0.1',
+      version='1.0.2',
       description='Simple wrapper for Python (SMTP) mail using Jinja2 templates',
       author='Chris',
       url='https://github.com/chris-ca/simplemailer',
